759d9d99e27affa51b3cb97568390789a70229c8
Aaaaaaajiandah <517010@bsd48.org>
2025-01-29 17:01:55 -0800

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/active/go/',
+    prefix: '/UV-Static/active/go/',
     bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/active/uv/uv.handler.js',
-    bundle: '/active/uv/uv.bundle.js',
-    config: '/active/uv/uv.config.js',
-    sw: '/active/uv/uv.sw.js',
+    handler: '/UV-Static/active/uv/uv.handler.js',
+    bundle: '/UV-Static/active/uv/uv.bundle.js',
+    config: '/UV-Static/active/uv/uv.config.js',
+    sw: '/UV-Static/active/uv/uv.sw.js',
 };
